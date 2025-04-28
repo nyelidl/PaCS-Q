8f0724bcc8589aacf1db8bf7d2094f9b88effe0c
nyelidl <120070650+nyelidl@users.noreply.github.com>
2025-04-28 10:36:46 +0900

    Add files via upload

---
+++ tests/test.py
@@ -0,0 +1,9 @@
+#import matplotlib
+#matplotlib.use("TkAgg")  # or "Qt5Agg" if TkAgg doesn't work for you
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3, 4])
+plt.ylabel('some numbers')
+plt.show()
+
+# MDanalysis(pip), tkinter(apt-get, python3-tk)
---
+++ tests/test.sh
@@ -0,0 +1,4 @@
+parm ../../../complex.top
+trajin qmmm.nc 11 11 1
+trajout min.rst
+go
